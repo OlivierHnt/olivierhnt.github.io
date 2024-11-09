ecbf15e1df1ab995b44dfef2b293ecabf947aefa
OlivierHnt <38465572+OlivierHnt@users.noreply.github.com>
2024-11-10 01:45:01 +0800

    Update

--- script.js
+++ script.js
@@ -3,12 +3,20 @@
 const burger = document.getElementById("burgerIcon");
 const dropdown = document.getElementById("dropdownContent");
 
-const isOpen = burger.classList.contains('open');
-
-function toggleDropdown() {
+// Toggle menu when clicking the burger icon
+burger.addEventListener('click', (event) => {
+    event.stopPropagation(); // Prevent click from propagating to the document
+    dropdown.classList.toggle('open');
     burger.classList.toggle('open');
-    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
-}
+  });
+
+// Close the menu if clicking outside the burger icon or menu
+document.addEventListener('click', (event) => {
+    if (!burger.contains(event.target) && !dropdown.contains(event.target)) {
+      dropdown.classList.remove('open');
+      burger.classList.remove('open');
+    }
+  });
 
 //
 
